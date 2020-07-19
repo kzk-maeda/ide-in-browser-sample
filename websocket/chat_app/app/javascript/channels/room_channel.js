3f7957cb786b93c87fef75dbed47988318a06433
kzk-maeda <casino.music0711@gmail.com>
2020-07-20 08:32:05 +0900

    channel message is added

--- websocket/chat_app/app/javascript/channels/room_channel.js
+++ websocket/chat_app/app/javascript/channels/room_channel.js
@@ -3,14 +3,16 @@ import consumer from "./consumer"
 const chatChannel = consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log('connected');
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    console.log('disconnected');
   },
 
   received: function(data) {
-  return alert(data['message']);
+    return alert(data['message']);
   },
 
   speak: function(message) {
