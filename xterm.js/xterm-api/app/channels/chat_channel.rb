7aced7eaf9adf67d750aa5f94f5467047f4d1e7c
kzk-maeda <casino.music0711@gmail.com>
2020-07-20 02:57:50 +0900

    web socket basic project is created

---
+++ xterm.js/xterm-api/app/channels/chat_channel.rb
@@ -0,0 +1,14 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from "chat:message"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def put_message
+    ChatChannel.broadcast_to('message', 'hello')
+  end
+end
